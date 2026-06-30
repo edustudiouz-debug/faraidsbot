07498c39057fab9c7ba8bebe7ed7373c9c724510
edustudiouz-debug <edustudiouz@gmail.com>
2026-06-30 21:36:44 +0500

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-from . import pdf_generator
-from . import scheduler
 
-# Loyihaning boshqa qismlari (masalan, main.py yoki admin.py) 
-# utils ichidagi funksiyalarni muammosiz topishi uchun eksport qilamiz
-__all__ = ["pdf_generator", "scheduler"]
